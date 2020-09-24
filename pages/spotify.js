ec06cdff6e3301835569cda03ca02c2e4f38f0c6
Daniel Nguyen <danngu@uw.edu>
2020-09-24 10:17:44 -0700

    oop forgot spotify

--- pages/spotify.js
+++ pages/spotify.js
@@ -8,7 +8,13 @@ import Product from '../components/spotify/product';
 export default function Home() {
 	return (
 		<div>
-			<Head></Head>
+			<Head>
+				<title>Spotify Podcasts</title>
+				<meta
+					name='viewport'
+					content='initial-scale=1.0, width=device-width'
+				/>
+			</Head>
 			<Nav></Nav>
 			<Layout>
 				<Hero></Hero>
